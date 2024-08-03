cfed60fc4e18bba45a99a9be7d78fd2629647fc3
arifinlenna <arifin@lenna.ai>
2024-08-03 09:00:08 +0700

    logger

--- config/Logger.go
+++ config/Logger.go
@@ -1,7 +1,9 @@
 package config
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"time"
@@ -10,22 +12,49 @@ import (
 	"github.com/gofiber/fiber/v2/middleware/logger"
 )
 
+func generalLogFile() *os.File {
+	generalLogFile, err := os.OpenFile("./storage/logs/general_log/"+time.Now().Format("01-02-2006")+".log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0777)
+    if err != nil {
+		log.Fatalf("error opening file: %v", err)
+    }
+	return generalLogFile
+}
+
 func Logger(app *fiber.App) {
 	createDirStorageLogs()
+
+	generalLogFile := generalLogFile()
+
+	currentDate := time.Now().Format("01-02-2006")
 	file, err := os.OpenFile("./storage/logs/"+time.Now().Format("01-02-2006")+".log", os.O_RDWR|os.O_CREATE|os.O_APPEND, 0777)
 	if err != nil {
 		panic(fmt.Sprintf("error opening file: %v", err))
 	}
+	app.Use(func(c *fiber.Ctx) error {
+		// Simpan body asli untuk digunakan nanti
+		body := c.Body()
+		var bodyJSON interface{}
+		if err := json.Unmarshal(body, &bodyJSON); err == nil {
+			compactBody, _ := json.Marshal(bodyJSON)
+			c.Locals("body", string(compactBody))
+		} else {
+			c.Locals("body", string(body))
+		}
+		
+		return c.Next()
+	})
+
 	app.Use(logger.New(logger.Config{
-		Output: file,
-		Format: "body : ${body} | queryParams : ${queryParams} | reqHeaders : ${reqHeaders} | ${time} | ${status} | ${latency} | ${ip} | ${method} | url : ${url} | path : ${path} | route : ${route} | ${error}\n",
+		Output: io.MultiWriter(file, generalLogFile),
+		Format: fmt.Sprintf("body : ${locals:body} | queryParams : ${queryParams} | reqHeaders : ${reqHeaders} | time : ${time} | date : %s | status : ${status} | responseTime : ${latency} | ip : ${ip} | ${method} | url : ${url} | path : ${path} | route : ${route} | error : ${error}\n",currentDate),
 	}))
-	log.SetOutput(file)
+	
+	log.SetOutput(generalLogFile)
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 }
 
 func createDirStorageLogs() {
-	dir := "./storage/logs"
+	dir := "./storage/logs/general_log"
 	if _, err := os.Stat(dir); os.IsNotExist(err) {
 		err := os.MkdirAll(dir, 0777)
 		if err != nil {
